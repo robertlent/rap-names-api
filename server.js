300bf29a7285483249bc9d832058bde484a47d09
Robert Lent <robert@therobertlent.com>
2022-10-08 20:34:38 -0700

    Fixed cors errors

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const {
 } = require('express')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage': {
