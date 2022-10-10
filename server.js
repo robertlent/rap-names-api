ee057d1f58115a77db1931631bbad91b8ee7b4ec
Robert Lent <robert@therobertlent.com>
2022-10-09 19:46:07 -0700

    Change age attribute to dob, add more rappers to object

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-const {
-    response
-} = require('express')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -10,19 +7,54 @@ app.use(cors())
 
 const rappers = {
     '21 savage': {
-        'age': 29,
+        'dob': '10/22/1992',
         'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'
+        'birthLocation': 'Plaistow, London, England'
     },
     'chance the rapper': {
-        'age': 29,
+        'dob': '4/16/1993',
         'birthName': 'Chancelor Bennett',
         'birthLocation': 'Chicago, Illinois'
     },
-    'dylan': {
-        'age': 29,
-        'birthName': 'Dylan',
-        'birthLocation': 'Dylan'
+    'snoop dogg': {
+        'dob': '10/20/1971',
+        'birthName': 'Calvin Cordozar Broadus Jr',
+        'birthLocation': 'Long Beach, California'
+    },
+    'ice cube': {
+        'dob': '6/15/1969',
+        'birthName': 'O\'Shea Jackson',
+        'birthLocation': 'Los Angeles, California'
+    },
+    'eminem': {
+        'dob': '10/17/1972',
+        'birthName': 'Marshall Bruce Mathers III',
+        'birthLocation': 'St. Joseph, Missouri'
+    },
+    'jay-z': {
+        'dob': '12/4/1969',
+        'birthName': 'Shawn Corey Carter',
+        'birthLocation': 'New York, New York'
+    },
+    '50 cent': {
+        'dob': '7/6/1975',
+        'birthName': 'Curtis James Jackson III',
+        'birthLocation': 'Queens, New York'
+    },
+    'lil wayne': {
+        'dob': '9/27/1982',
+        'birthName': 'Dwayne Michael Carter Jr',
+        'birthLocation': 'New Orleans, Louisiana'
+    },
+    'drake': {
+        'dob': '10/24/1986',
+        'birthName': 'Aubrey Drake Graham',
+        'birthLocation': 'Toronta, Ontario, Canada'
+    },
+    'unknown': {
+        'dob': '1/1/1970',
+        'birthName': 'Unknown',
+        'birthLocation': 'Unknown'
     },
 }
 
@@ -34,13 +66,21 @@ app.get('/js/main.js', (req, res) => {
     res.sendFile(__dirname + '/js/main.js')
 })
 
+app.get('/css/normalize.css', (req, res) => {
+    res.sendFile(__dirname + '/css/normalize.css')
+})
+
+app.get('/css/style.css', (req, res) => {
+    res.sendFile(__dirname + '/css/style.css')
+})
+
 app.get('/api/:rapperName', (req, res) => {
     const rappersName = req.params.rapperName.toLowerCase()
 
     if (rappers[rappersName]) {
         res.json(rappers[rappersName])
     } else {
-        res.json(rappers['dylan'])
+        res.json(rappers['unknown'])
     }
 })
 
